6270991468104e41317f1309aacaa6991cbdceb7
wbglaeser <ben.glaeser@tuta.io>
2020-04-20 10:58:41 +0200

    update measurement stations json and add rudimentary app routes

--- app.py
+++ app.py
@@ -2,5 +2,35 @@ import json
 
 from flask import Flask, render_template
 
+from packages.measurement_levels import MeasurementLevels
+
 # setup minimal flask app
 app = Flask(__name__)
+
+# retrieve measurement stations
+with open("measurement_stations.json", "r") as fp:
+    measurement_stations = json.load(fp)
+
+
+# define app routes
+@app.route("/", methods=['GET', 'POST'])
+def welcome():
+
+    msg = """
+    Hi, falls du nach den Pegelständen in und um Passau suchst, versuch es mal mit folgender URL:
+    http://localhost:5000/water_levels
+    """
+    return msg
+
+# define app routes
+@app.route("/water_levels", methods=['GET', 'POST'])
+def measurement_levels():
+
+    # retrieve latest measurements
+    current_measurements = MeasurementLevels.fetch_all_stations(measurement_stations)
+    print(current_measurements)
+
+    return 0
+
+if __name__ == "__main__":
+    app.run()
